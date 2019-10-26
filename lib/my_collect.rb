fbc996d52ecc6461f9c129d70ade4521a5354a3d
essential-robot-4364 <brandon.james414@gmail.com>
2019-10-26 17:48:22 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,8 +2,8 @@ def my_collect(empty_array)
   b = 0
   
   while b < empty_array.length 
-    yield (empty_array[b]) do |name|
-      name.split(" ").first
+    yield (empty_array[b]) do |empty_array|
+      empty_array.split(" ").first
     b = b + 1 
     
   end
