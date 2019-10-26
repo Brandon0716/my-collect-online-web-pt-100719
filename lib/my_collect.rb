1da449e98950d89ef71770f45e9d8fc2d2b39a04
essential-robot-4364 <brandon.james414@gmail.com>
2019-10-26 17:53:40 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,9 +1,9 @@
 def my_collect(empty_array)
   b = 0
-  array =[]
+  array = []
   
   while b < empty_array.length 
-   array << yield (empty_array[b]) 
+    array << yield (empty_array[b]) 
     b = b + 1 
     
   end
